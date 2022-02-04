c2b4638cf2c398a92c2c7e25bc804f62a650682c
TanyaRan <tatyanaran@gmail.com>
2022-02-05 00:15:32 +0200

    Draw gale cells, add command buttons

--- main.py
+++ main.py
@@ -12,6 +12,17 @@ app_running = True
 # 1. Задаваме размерите му
 size_canvas_x = 600
 size_canvas_y = 600
+# размер на игралното поле в клетки
+s_x = s_y = 10
+# размер на стъпката между линиите, те ще са 11 на брой, защото клетките са 10
+step_x = size_canvas_x // s_x
+step_y = size_canvas_y // s_y
+# Коригираме размера на полето - за всякъв брой клетки
+size_canvas_x = step_x * s_x
+size_canvas_y = step_y * s_y
+
+# Добавяме област за командване на играта
+menu_x = 250
 
 def on_closing():
     # Тук ще променяме app_running, затова ще кажем че тя е глобална
@@ -32,13 +43,35 @@ tk.resizable(0, 0)
 tk.wm_attributes("-topmost", 1)
 
 # Вече можем да създадем прозореца(canvas) на играта:
-canvas = Canvas(tk, width=size_canvas_x, height=size_canvas_x, bd=0 ,highlightthickness=0)
+canvas = Canvas(tk, width=size_canvas_x + menu_x, height=size_canvas_x, bd=0 ,highlightthickness=0)
 # 1. На прозореца създаваме правоъгълник с бял цвят
 canvas.create_rectangle(0, 0, size_canvas_x, size_canvas_y, fill="white")
 # 2. Опаковаме канваса и обновяваме tk(tk - това е обектът, който описва нашата игра)
 canvas.pack()
 tk.update()
 
+# Създаваме клетките на полето
+def draw_table():
+    for i in range(0, s_x + 1):
+        # creates line from x1,y1 to x2,y2
+        canvas.create_line(step_x * i, 0, step_x * i, size_canvas_y)
+    for i in range(0, s_y + 1):
+        canvas.create_line(0, step_y * i, size_canvas_x, step_y * i)
+
+
+def button_show_enemy():
+    pass
+
+def button_begin_again():
+    pass
+
+b0 = Button(tk, text="Покажи корабите на противника", command = button_show_enemy)
+b0.place(x = size_canvas_x + 20, y = 30)
+
+b1 = Button(tk, text="Започни отначало!", command = button_begin_again)
+b1.place(x = size_canvas_x + 20, y = 70)
+
+draw_table()
 
 # Създаваме цикъла на работa на играта
 while app_running:
